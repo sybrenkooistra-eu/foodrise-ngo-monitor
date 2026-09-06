3a89dd5d2893b221e572391362ab793e64bd57fd
sybrenkooistra-eu <sybrenkooistra@gmail.com>
2026-09-06 18:07:38 +0200

    Voeg Inside Climate News toe

--- scraper.py
+++ scraper.py
@@ -127,6 +127,10 @@ OPINION_SOURCES = [
      "link_pattern": r"freedomfoodalliance\.org/unfork-the-food-system/[a-z0-9-]{5,}",
      "exclude_pattern": r"^$"},
 
+    {"name": "Inside Climate News (EN)",
+     "type": "rss",
+     "url": "https://insideclimatenews.org/feed/"},
+
     {"name": "Mongabay (EN)",
      "type": "rss",
      "url": "https://news.mongabay.com/feed/"},
